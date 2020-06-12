dd694e82874ed129a851c0b5f4be27f68c7cf68c
YangHui <tlz.yh@outlook.com>
2020-06-12 19:23:27 +0800

    add

---
+++ AssetStudioExporter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetStudio
+{
+    static class Program
+    {
+        static void Main()
+        {
+            AssetsManager assetsManager = new AssetsManager();
+            string[] files = { "F:\\Bladed\\chopghost_Data\\StreamingAssets\\shared" };
+            assetsManager.LoadFiles(files);
+        }
+    }
+}
